4deb4e2520eda4f8e541b7296beb72a13d406283
bobbyhadz <opmftw@gmail.com>
2024-03-02 10:39:16 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Assigning to rvalue error in JavaScript
+
+// EXAMPLE 1 - Using a single equal sign `=` instead of double or triple equals
+
+// ✅ correct
+if (10 === 10) {
+  console.log('success');
+}
+
+// ------------------------------------------------------------------
+
+// // EXAMPLE 2 - Using dot notation with object properties that contain hyphens
+
+// // ✅ correct
+// const obj = {};
+
+// obj['background-color'] = 'green';
+
+// console.log(obj);
